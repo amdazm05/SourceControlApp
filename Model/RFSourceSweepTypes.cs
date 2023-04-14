e87cdda6700f0cec8ff92a5dc98b4ae6bc03c1f1
amdazm05 <amdali1998@yahoo.com>
2023-04-14 17:23:08 +0500

    initial commit

---
+++ Model/RFSourceSweepTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFSourceControllerApp.Model
+{
+    class RFSourceSweepTypes
+    {
+        public string SweepType
+        {
+            get
+            {
+                return null;
+            }
+
+            set
+            {
+
+            }
+        }
+        RFSourceSweepTypes()
+        {
+
+        }
+
+        public void setRFSweepType(string sweepType)
+        {
+
+        }
+    
+    }
+}
