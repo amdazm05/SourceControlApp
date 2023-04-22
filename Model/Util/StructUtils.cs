c0b8d63905a063a170bd22c2f5fd04a15a9a1556
amdazm05 <amdali1998@yahoo.com>
2023-04-22 21:02:04 +0500

    feat: added a serialisation and deserialisation class for structures

---
+++ Model/Util/StructUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+//See the useful reference here
+/// <summary>
+/// https://stackoverflow.com/questions/628843/byte-for-byte-serialization-of-a-struct-in-c-sharp
+/// </summary>
+namespace RFSourceControllerApp.Model.Util
+{
+    class StructUtils
+{
+        /// <summary>
+        /// converts byte[] to struct
+        /// </summary>
+        public static T RawDeserialize<T>(byte[] rawData, int position)
+        {
+            int rawsize = Marshal.SizeOf(typeof(T));
+            if (rawsize > rawData.Length - position)
+                throw new ArgumentException("Not enough data to fill struct. Array length from position: " + (rawData.Length - position) + ", Struct length: " + rawsize);
+            IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+            Marshal.Copy(rawData, position, buffer, rawsize);
+            T retobj = (T)Marshal.PtrToStructure(buffer, typeof(T));
+            Marshal.FreeHGlobal(buffer);
+            return retobj;
+        }
+
+        /// <summary>
+        /// converts a struct to byte[]
+        /// </summary>
+        public static byte[] RawSerialize(object anything)
+        {
+            int rawSize = Marshal.SizeOf(anything);
+            IntPtr buffer = Marshal.AllocHGlobal(rawSize);
+            Marshal.StructureToPtr(anything, buffer, false);
+            byte[] rawDatas = new byte[rawSize];
+            Marshal.Copy(buffer, rawDatas, 0, rawSize);
+            Marshal.FreeHGlobal(buffer);
+            return rawDatas;
+        }
+    }
+}
